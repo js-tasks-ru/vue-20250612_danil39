519f5e0a895f8ad73722a0916977dc00287b5de1
Danil <musikhin.danil@gmail.com>
2025-07-07 16:20:52 +0500

    Задача 1.0: createApp

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,22 @@
 import { defineComponent, createApp } from 'vue'
+
+const Comp = defineComponent({
+    setup() {
+        const today = new Date();
+
+        function formatAsLocalDate(date) {
+            return new Date(date).toLocaleDateString('ru-RU', {
+                dateStyle: 'long'
+            });
+        }
+
+        return {
+            formatAsLocalDate: formatAsLocalDate(today)
+        }
+    },
+
+    template: '<div>Сегодня {{ formatAsLocalDate }}</div>'
+});
+
+const app = createApp(Comp);
+const vm = app.mount('#app');
