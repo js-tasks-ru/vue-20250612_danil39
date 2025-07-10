1a8f09c77559f4a494c22ddb72f1ce287ddab59a
Danil <musikhin.danil@gmail.com>
2025-07-10 13:54:09 +0500

    02-basics-2/40-marked-emails/ Задание выполнено

--- 02-basics-2/40-marked-emails/MarkedEmailsApp.js
+++ 02-basics-2/40-marked-emails/MarkedEmailsApp.js
@@ -32,19 +32,30 @@ export const emails = [
 export default defineComponent({
   name: 'MarkedEmailsApp',
 
-  setup() {},
+  setup() {
+    const search = ref(""); 
+
+    const markedEmails = computed(() => {
+      return emails.map(email => ({
+        email,
+        marked: search.value !== '' && email.includes(search.value)
+      }))
+    }) 
+
+    return {
+      search,
+      markedEmails,
+    }
+  },
 
   template: `
     <div>
       <div class="form-group">
-        <input type="search" aria-label="Search" />
+        <input type="search" aria-label="Search" v-model="search" />
       </div>
       <ul aria-label="Emails">
-        <li>
-          Eliseo@gardner.biz
-        </li>
-        <li class="marked">
-          Jayne_Kuhic@sydney.com
+        <li v-for="email in markedEmails" :class="{ marked: email.marked }">
+          {{ email.email }}
         </li>
       </ul>
     </div>
