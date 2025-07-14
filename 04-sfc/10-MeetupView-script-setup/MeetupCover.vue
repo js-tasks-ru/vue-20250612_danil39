a65c904de554b58c4c643c4d07675fb3296451f4
Danil <musikhin.danil@gmail.com>
2025-07-14 16:03:56 +0500

    04-sfc/10-MeetupView-script-setup/ Выполнен

--- 04-sfc/10-MeetupView-script-setup/MeetupCover.vue
+++ 04-sfc/10-MeetupView-script-setup/MeetupCover.vue
@@ -1,26 +1,17 @@
-<script>
+<script setup>
 import { computed, defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'MeetupCover',
-
-  props: {
-    title: {
-      type: String,
-    },
-
-    image: {
-      type: String,
-    },
+const props = defineProps({
+  title: {
+    type: String,
   },
 
-  setup(props) {
-    const bgStyle = computed(() => (props.image ? { '--bg-url': `url('${props.image}')` } : undefined))
-    return {
-      bgStyle,
-    }
+  image: {
+    type: String,
   },
-})
+});
+
+const bgStyle = computed(() => (props.image ? { '--bg-url': `url('${props.image}')` } : undefined))
 </script>
 
 <template>
