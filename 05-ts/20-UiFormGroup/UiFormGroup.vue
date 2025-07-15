777fefe6360d2461f124b945adb62b5ad7f38f30
Danil <musikhin.danil@gmail.com>
2025-07-15 14:43:59 +0500

    05-ts/20-UiFormGroup/ Выполнено

--- 05-ts/20-UiFormGroup/UiFormGroup.vue
+++ 05-ts/20-UiFormGroup/UiFormGroup.vue
@@ -1,13 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, useSlots, type VNode } from 'vue';
+
+const props = defineProps<{
+  for?: string,
+  label?: string, 
+  description?: string,
+  hint?: string,
+  showHint?: boolean,
+  invalid?: boolean
+}>()
+
+defineSlots<{
+  label?: () => VNode[],
+  description?: () => VNode[],
+  default: () => VNode[],
+}>()
+
+const slots = useSlots()
+
+const showHint = computed(() => 
+  props.hint !== undefined && (props.showHint || props.invalid)
+)
+</script>
 
 <template>
   <div class="form-group">
-    <div class="form-group__label-wrapper">
-      <label for="FOR" class="form-group__label">LABEL</label>
-      <div class="form-group__description">DESCRIPTION</div>
+    <div class="form-group__label-wrapper" v-if="props.label || slots.label || props.description || slots.description">
+      <label class="form-group__label" v-if="props.label || slots.label" :for="props.for">
+        <slot name="label">{{ props.label }}</slot>
+      </label>
+      <div class="form-group__description" v-if="props.description || slots.description">
+        <slot name="description">{{ props.description }}</slot>
+      </div>
     </div>
-    <div class="form-group__control">CONTENT</div>
-    <div class="form-group__hint form-group__hint--invalid">HINT | ERROR</div>
+
+    <div class="form-group__control">
+      <slot />
+    </div>
+    
+    <div class="form-group__hint" v-if="props.hint" :class="{'form-group__hint--invalid': props.invalid}">
+      <span v-if="showHint">{{ props.hint }}</span>
+    </div>
   </div>
 </template>
 
